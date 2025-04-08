1098627bd83f421853120617e8ab0bd1bc0b3998
Ashmit <ashmitmala002@gmail.com>
2025-04-09 00:41:30 +0530

    Open Expense Overlay

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+
 void main() {
-  runApp(const MaterialApp(
-    home: Expenses(),
-  ));
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Expenses()
+    ),
+  );
 }
