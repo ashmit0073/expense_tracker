99b0f47c4cd36e77eecd16574684d5fad1918dd2
Ashmit <ashmitmala002@gmail.com>
2025-04-10 19:51:39 +0530

    Add Theme to Expense List

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -10,16 +10,20 @@ class ExpenseItem extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
         child: Column(
+          // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(expense.title),
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
             const SizedBox(height: 4),
             Row(
               children: [
                 Text('₹${expense.amount.toStringAsFixed(2)}'),
-                const Spacer(flex: 1),
+                const Spacer(),
                 Row(
                   children: [
-                     Icon(categoryIcons[expense.category]),
+                    Icon(categoryIcons[expense.category]),
                     const SizedBox(width: 8),
                     Text(expense.formattedDate),
                   ],
